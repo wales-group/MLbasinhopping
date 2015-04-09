7f2b09677da5d71d9dc76996ae8cbc7d06f29a1d
Andrew Ballard <andy@ontoitsoftware.com>
2015-04-09 12:02:12 +0100

    cleaned up

--- potentials.py
+++ potentials.py
@@ -3,110 +3,6 @@ import theano
 import theano.tensor as T
 from pele.potentials import BasePotential
 from pele.systems import BaseSystem
-
-class BaseModel(object):
-    
-    def __init__(self,params0=None,points=None,x=None,npoints=100,sigma=0.3):        
-
-        self.params0 = params0
-        self.sigma = sigma
-        self.points = points
-        self.npoints = npoints     
-        
-        """ optional x-values to generate training data """
-        self.xvals = x
-        
-        if self.points == None:
-            assert self.params0 != None
-            self.nparams = len(self.params0)
-            self.points = self.generate_points(x=self.xvals)
-        else:
-            self.npoints = len(self.points)
-            
-    def generate_points(self,x=None):
-        
-        if x==None:
-            x = np.random.random(self.npoints)
-
-        y = self.model_batch(x,self.params0) + np.random.normal(scale=self.sigma,size=self.npoints)
-        
-        return zip(x,y)
-    
-class WaveModel(BaseModel):
-
-    def __init__(self,*args,**kwargs):
-
-        super(WaveModel,self).__init__(*args,**kwargs)
-        self.xvals = 3*np.pi*np.random.random(self.npoints)
-        self.points = self.generate_points(x=self.xvals)
-        
-    def model(self,x,params):
-        return np.exp(-params[0]*x) * np.sin(params[1]*x+params[2]) * np.sin(params[3]*x+params[4])
-    
-    def model_batch(self, x, params):
-        """evaluate multiple data points at once
-        
-        this can be much faster than doing them individually
-        """
-
-        return np.exp(-params[0]*x) * np.sin(params[1]*x + params[2]) * np.sin(params[3]*x + params[4])
-        #return np.exp(-params[0]*x) * np.sin(params[1]*x + params[2])
-
-    def model_gradient_batch(self, x, params):
-        """return a matrix of gradients at each point
-        
-        Returns
-        -------
-        grad : array
-            grad[i,j] is the gradient w.r.t. param[i] at point[j]
-        """
-        t1 = np.exp(-params[0]*x)
-        t2 = np.sin(params[1]*x+params[2])
-        t2der = np.cos(params[1]*x+params[2])
-        t3 = np.sin(params[3]*x+params[4])
-        t3der = np.cos(params[3]*x+params[4])
-        
-        grad = np.zeros([params.size, x.size])
-        grad[0,:] = -x * t1 * t2 * t3
-        
-        grad[1,:] = x * t1 * t2der * t3
-        grad[2,:] = t1 * t2der * t3
-        grad[3,:] = x * t1 * t2 * t3der
-        grad[4,:] = t1 * t2 * t3der
-        
-        return grad
-
-class ErrorFunction(BasePotential):
-    """a quadratic error function for fitting
-    
-    V(xi,yi|alpha) = 0.5 * (yi-(f(xi|alpha))**2
-    where 
-    """
-    def __init__(self, model):
-        """ instance of model to fit"""
-        self.model = model
-
-    def getEnergy(self, params):
-        
-#         cost = self.model.cost(params)
-        return self.model.evaluated_cost(params)
-#         return cost.eval()
-
-    def getEnergyGradient(self, params):
-
-        return self.model.evaluated_costGradient(params)
-#         print self.model.evaluated_costGradient(params)
-#         exit()
-#         e = self.model.cost(params)
-#         g = self.model.costGradient(params)
-# 
-#         return e.eval(), g.eval()
-    
-    def getEnergyGradientHessian(self, params):
-        
-        e, g, h = self.model.costGradientHessian(params)
-        return e.eval(), g.eval(), h.eval()
-    
                    
 class BaseTheanoModel(object):
     """ This is an example class for playing around with regression modelling using theano.
@@ -152,21 +48,11 @@ class BaseTheanoModel(object):
         self.params.set_value(params)
         return self._theano_costGradientHessian()
             
-    def predict2(self, xval):
+    def predict(self, xval):
         
         self.x_to_predict.set_value(xval)    
         
         return self.Y(self.x_to_predict).eval()
-        
-    def predict(self, xval):
-
-        self.x_to_predict.set_value(xval)
-#         xval = T.scalar("x")        
-        model = self.params * self.x_to_predict
-        model.eval()
-        ret = theano.function([], model)
-        
-        return ret()
     
     def Y(self, X):
         """ 
@@ -175,9 +61,7 @@ class BaseTheanoModel(object):
         returns : Theano shared variable
         """
         raise NotImplementedError
-    
-        return T.exp(-self.params[0]*X) * T.sin(self.params[1]*X+self.params[2]) \
-                    * T.sin(self.params[3]*X + self.params[4])
+
         
     def costFunction(self):
 #         print self.params.shape
@@ -185,36 +69,6 @@ class BaseTheanoModel(object):
         Cost = T.sum((self.Y(self.X) - self.t)**2)
         
         return Cost
-    
-#     def costGradient(self):
-#         
-# #         self.params.set_value()
-#         
-#         g = T.grad(self.cost(), self.params)
-#         return g
-    
-#     def costGradientHessian(self, params):
-#         
-#         c, g = self.costGradient(params)
-#         h = theano.gradient.hessian(c, self.params)
-#         
-#         return c, g, h
-
-
-#     def evaluated_cost(self, params):
-#         
-# #         func = theano.function([],self.cost(params))
-#         self.params.set_value(params)
-#         return self.theano_cost()
-# 
-#     def evaluated_costGradient(self, params):
-# #         c = self.evaluated_cost(params)
-# #         g = theano.function([], self.costGradient(params))
-#         self.params.set_value(params)
-# #         print c, g
-#         return self.theano_costGradient()
-    
-#         costfunction = 
 
 class TestModel(BaseTheanoModel):
     
@@ -248,26 +102,21 @@ class RegressionSystem(BaseSystem):
         super(RegressionSystem, self).__init__()
         self.model = model
         self.params.database.accuracy =0.01
-        self.params.double_ended_connect.local_connect_params.tsSearchParams.hessian_diagonalization = True
+#         self.params.double_ended_connect.local_connect_params.tsSearchParams.hessian_diagonalization = True
 
     def get_potential(self):
         return RegressionPotential(self.model)
     
     def get_mindist(self):
         # no permutations of parameters
-        
         #return mindist_with_discrete_phase_symmetry
         #permlist = []
-        return lambda x1, x2: np.linalg.norm(x1-x2)
-#         return MinPermDistWaveModel( niter=10)
+        return lambda x1, x2: (np.linalg.norm(x1-x2), x1, x2)
 
     def get_orthogonalize_to_zero_eigenvectors(self):
         return None
-#         return my_orthog_opt
-        #return orthogopt
-        #return orthogopt_translation_only
     
-    def get_minimizer(self, tol=1.0e-2, nsteps=100000, M=4, iprint=1, maxstep=1.0, **kwargs):
+    def get_minimizer(self, tol=1.0e-6, nsteps=100000, M=4, iprint=0, maxstep=1.0, **kwargs):
         from pele.optimize import lbfgs_cpp as quench
         return lambda coords: quench(coords, self.get_potential(), tol=tol, 
                                      nsteps=nsteps, M=M, iprint=iprint, 
@@ -279,17 +128,70 @@ class RegressionSystem(BaseSystem):
     def get_compare_exact(self, **kwargs):
         # no permutations of parameters
         mindist = self.get_mindist()
-        return lambda x1, x2: (mindist(x1, x2)[0] < 1e-3, x1, x2)
+        return lambda x1, x2: mindist(x1, x2)
+
+def run_basinhopping(system, xvals, tvals):
+    import matplotlib.pyplot as plt
+    
+    database = system.create_database()
+    x0 = np.random.random(system.model.params.eval().size)
+    
+    from pele.takestep import RandomCluster
+    step = RandomCluster(volume=5.0)
+    bh = system.get_basinhopping(database=database, takestep=step,coords=x0,temperature = 10.0)
+    #bh.stepsize = 20.
+    bh.run(500)
+    print "found", len(database.minima()), "minima"
+    min0 = database.minima()[0]
+    print "lowest minimum found has energy", min0.energy
+    m0 = database.minima()[0]
+    mindist = system.get_mindist()
+    for m in database.minima():
+        dist = mindist(m0.coords, m.coords.copy())[0]
+        print "   ", m.energy, dist, m.coords
+
+
+#     for m in database.minima():
+#         system.model.params.set_value(m.coords)
+#         curve = system.model.predict(np.arange(0.,3.0*np.pi,0.01))
+#         plt.plot(np.arange(0.,3.0*np.pi,0.01), curve, '-')
+    
+#     plt.plot(xvals,tvals,'x')
+#     plt.show()
+    
+    return system, database
+
+def run_double_ended_connect(system, database):
+    # connect the all minima to the lowest minimum
+    from pele.landscape import ConnectManager
+    manager = ConnectManager(database, strategy="gmin")
+    for i in xrange(database.number_of_minima()-1):
+        min1, min2 = manager.get_connect_job()
+        connect = system.get_double_ended_connect(min1, min2, database)
+        connect.connect()
+        
+def make_disconnectivity_graph(database):
+    from pele.utils.disconnectivity_graph import DisconnectivityGraph, database2graph
+    import matplotlib.pyplot as plt
+    
+    graph = database2graph(database)
+    dg = DisconnectivityGraph(graph, nlevels=10, center_gmin=True)
+    dg.calculate()
+    dg.plot()
+    plt.show()
     
 def main():
+#     import matplotlib.pyplot as plt
     
     theano.config.mode = 'FAST_RUN'
-    xvals = np.random.random((100,1))
+    xvals = 3.0*np.pi*np.random.random((100,1))
     xvals = np.atleast_2d(xvals)
 #     real_params = np.array([1.0, 0.1, 2.0])
     real_params=np.array([0.1,1.0,0.0,0.0,0.5*np.pi])
     tvals = np.exp(-real_params[0]*xvals) * np.sin(real_params[1]*xvals+real_params[2]) \
-                    * np.sin(real_params[3]*xvals + real_params[4])
+                    * np.sin(real_params[3]*xvals + real_params[4]) 
+                   
+    tvals = tvals + np.random.normal(scale=0.1,size=(100,1))
 #     real_params[0] * np.exp(real_params[1] * xvals) * np.sin(real_params[2] * xvals) \
 #                 + 0.1 * np.random.random((100,1))
 
@@ -303,18 +205,28 @@ def main():
     system = RegressionSystem(model)
     pot = system.get_potential()
     
-    coords = np.random.random(starting_params.shape)
+    system, db = run_basinhopping(system, xvals, tvals)
+    run_double_ended_connect(system, db)
+    make_disconnectivity_graph(db)
+    exit()
+    
+    coords = 5.0*np.random.random(starting_params.shape)
 
     quench = system.get_minimizer()
-    for _ in xrange(100):
+    for _ in xrange(2):
+        coords = np.random.random(starting_params.shape)
 #         print model.theano_costGradient(coords)[0]
 #         print model.theano_costGradient(coords)[1]
 #         ret = pot.getEnergyGradient(coords)
 #         print ret
 #         exit()
         ret = quench(coords)
-        print ret.coords, ret.energy
-
+        model.params.set_value(ret.coords)
+        curve = model.predict(np.arange(0.,xvals.max(),0.01))
+        plt.plot(np.arange(0.,xvals.max(),0.01), curve, '-')
+    
+    plt.plot(xvals,tvals,'x')
+    plt.show()
 if __name__=="__main__":
     main()
         
