6f244c6854655337b2ee00dfa1bc99ec78243456
Andrew Ballard <andy@ontoitsoftware.com>
2015-04-09 12:27:11 +0100

    add some documentation

--- potentials.py
+++ potentials.py
@@ -5,7 +5,9 @@ from pele.potentials import BasePotential
 from pele.systems import BaseSystem
                    
 class BaseTheanoModel(object):
-    """ This is an example class for playing around with regression modelling using theano.
+    """ This is the Base Model class for model parameter estimation,
+     used in conjunction with basin-hopping for model parameter estimation.
+     Most of the objects in this class are theano functions. 
         We would eventually like to abstracify this, so that one can input
         a regression model expression string.
     """
@@ -37,35 +39,49 @@ class BaseTheanoModel(object):
                                                           )
         
     def theano_cost(self, params):
+        """ inputs: parameter values (np array)
+            returns: value of cost function
+        """
         self.params.set_value(params)
         return self._theano_cost()
     
     def theano_costGradient(self, params):
+        """ inputs: parameter values (np array)
+            returns: value of cost function and gradient
+        """
         self.params.set_value(params)
         return self._theano_costGradient()
     
     def theano_costGradientHessian(self, params):
+        """ inputs: parameter values (np array)
+            returns: value of cost function, gradient and hessian
+        """
         self.params.set_value(params)
         return self._theano_costGradientHessian()
             
     def predict(self, xval):
-        
+        """ inputs: xvals - array-like 
+            Returns: cost and gradient (np array)
+        """
+       
         self.x_to_predict.set_value(xval)    
         
         return self.Y(self.x_to_predict).eval()
     
     def Y(self, X):
         """ 
-        This defines the model
-        inputs : X -- theano shared variable
-        returns : Theano shared variable
+            This function defines the model
+            inputs : X -- theano shared variable
+            returns : theano shared variable
         """
         raise NotImplementedError
 
         
     def costFunction(self):
-#         print self.params.shape
-#         self.params.set_value(params)
+        """ 
+            This function defines the model cost function, default is a sum of squared errors
+            returns : theano shared variable
+        """
         Cost = T.sum((self.Y(self.X) - self.t)**2)
         
         return Cost
