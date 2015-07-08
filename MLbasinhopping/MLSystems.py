3c5baa309fca54d7ce79d128b9608af4c592be2d
Andrew Ballard <andy@ontoitsoftware.com>
2015-07-08 11:31:25 +0100

    cleanup

--- MLbasinhopping/MLSystems.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import theano
-import theano.tensor as T
-from pele.systems import BaseSystem
-
-class MLSystem(BaseSystem):
-    def __init__(self, inputs, targets):
-        self.inputs = inputs
-        self.targets = targets        
